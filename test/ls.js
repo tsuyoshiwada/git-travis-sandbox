cd759d449b86dacf29c9fc5e6741dee8535e70f7
tsuyoshiwada <tsuyoshi_wada@udstudio.co.jp>
2016-03-01 18:49:21 +0900

    Fix ls tests

--- test/ls.js
+++ test/ls.js
@@ -6,23 +6,19 @@ const ls = require("../ls");
 describe("ls tests", () => {
   it("should be return file list", () => {
     const files = ls();
-    const expected = [
-      "README.md",
-      "index.js",
-      "ls.js",
-      "node_modules",
-      "package.json",
-      "test"
-    ];
-    assert.deepStrictEqual(files, expected);
+    assert(files.length === 6);
+    assert(files.indexOf("README.md") > -1);
+    assert(files.indexOf("index.js") > -1);
+    assert(files.indexOf("ls.js") > -1);
+    assert(files.indexOf("node_modules") > -1);
+    assert(files.indexOf("package.json") > -1);
+    assert(files.indexOf("test") > -1);
   });
 
   it("should be return test/ file list", () => {
     const files = ls("test/");
-    const expected = [
-      "index.js",
-      "ls.js"
-    ];
-    assert.deepStrictEqual(files, expected);
+    assert(files.length === 2);
+    assert(files.indexOf("index.js") > -1);
+    assert(files.indexOf("ls.js") > -1);
   });
 });
